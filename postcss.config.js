cea7fd38c8cb6c44a5112289ff30bb0e817f8d58
Wesley Jones <wesjones2004@gmail.com>
2024-01-11 10:21:29 -1000

    Fix warnings

--- postcss.config.js
+++ postcss.config.js
@@ -2,8 +2,5 @@ module.exports = {
     plugins: [
         require("postcss-import"),
         require("autoprefixer"),
-        require("postcss-css-variables")({
-            preserve: true
-        }),
     ]
 };
